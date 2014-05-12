0e617785844b857c66deb1d5c17940f1f04f3919
deepakaj <deepakaj@gmail.com>
2014-05-12 04:33:17 +0400

    whoo yeah

--- plot3.R
+++ plot3.R
@@ -16,9 +16,9 @@ df <- read.csv.sql ('household_power_consumption.txt',Query,sep=';')
 plot2 <- as.numeric(df$Sub_metering_1)
 days <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
 
-plot(days, plot2, xlab="", ylab='Energy sub metering', type="l")
-lines(df$DateTime, df$Sub_metering_2)
-lines(df$DateTime, df$Sub_metering_3)
+plot(days, plot2, ylab='Energy sub metering', xlab="")
+lines(days, df$Sub_metering_2)
+lines(days, df$Sub_metering_3)
 
 ##Start drawing the PNG
 dev.copy(png, file = "c:/data/proj1/code/plot3.png", width = 480, height = 480, units = "px")
